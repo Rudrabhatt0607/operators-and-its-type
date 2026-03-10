2a40592e16dd78e17f471b9e40274eee33723526
Rudrabhatt0607 <r.m.bhatt8909@gmail.com>
2026-03-10 22:55:52 +0530

    arithmatic operators

---
+++ operators.py
@@ -0,0 +1,65 @@
+# Arithmetic Operators
+
+# Operator
+
+#          Operator        Meaning
+
+#          +              Add
+ 
+#          -              Subtract
+         
+#         *               Multiply
+
+#         /              Divide
+
+#         %               Modulo
+
+#        **               Exponentiation
+
+#       //               Floor Division
+
+
+A = 6 
+B = 5
+print(A+B)  #11  +              Add
+
+
+A = 6 
+B = 5
+print(A-B)  #1     -              Subtract
+
+
+
+A = 6 
+B = 5
+print(A*B)  #30     *               Multiply
+
+
+
+
+A = 6 
+B = 6
+print(A/B)  #1.0      /              Divide
+
+
+
+A = 11
+B = 6
+print(A%B)  # 5         %               Modulo
+
+
+
+A = 11
+B = 11
+print(A//B) # 1         //               Floor Division
+
+
+
+A = 4
+B = 2
+print(A**B)  #16            **               Exponentiation
+
+
+
+
+

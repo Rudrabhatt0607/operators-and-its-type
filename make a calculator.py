f2d49cfbd4dfc23ae3d3afe3c22f02fc79e6edc8
Rudrabhatt0607 <r.m.bhatt8909@gmail.com>
2026-03-11 17:41:55 +0530

    calculator done

---
+++ make a calculator.py
@@ -0,0 +1,25 @@
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":
+    print("Answer =", a + b)
+
+elif op == "-":
+    print("Answer =", a - b)
+
+elif op == "*":
+    print("Answer =", a * b)
+
+elif op == "/":
+    print("Answer =", a / b)
+
+else:
+    print("Invalid operator")
+    
+    
+    
+    
+    
